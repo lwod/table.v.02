13a61915141c5ac8504e2047bbf39d06a2bb4938
lwod <lwodcurrentmail@gmail.com>
2021-03-25 16:18:15 +0200

    add event click on map

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.json({extended:true}))
+
 app.get("/", async (req,res)=>{
 	res.json({
 		key:'value - 2'
@@ -11,5 +16,12 @@ app.get("/", async (req,res)=>{
 app.get('/table', async (req,res)=>{
 	res.sendFile(`${__dirname}/index.html`)
 })
+
+app.post("/table", async (req,res)=>{
+	
+	const {character} = req.body;
+	
+})
+
 
 app.listen(port);
