750bb63540448dda522f5164318c8a49bb7c226e
lwod <lwodcurrentmail@gmail.com>
2021-04-01 20:18:03 +0300

    add active-click marker

--- public/js/map.js
+++ public/js/map.js
@@ -9,4 +9,15 @@ function initMap(){
 	}
 	
 	let map = new google.maps.Map(document.getElementById('map'), options)
+	
+	const caroline = new google.maps.Marker({
+		map:map,
+		icon: 'https://raw.githubusercontent.com/lwod/tableV01/main/ceroline.png'
+	})
+	caroline.setPosition(new google.maps.LatLng(41.60066294542978,-87.33857699777955))
+
+
+	google.maps.event.addListener(map, 'click', (event)=>{
+		caroline.setPosition(new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()))
+	})
 }
